f758132b94164691de0a21f3ea2138b104b47f05
Wync <Tootie-@live.com.sg>
2022-06-02 00:20:22 +0800

    ...

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 
 
-export const startDate = new Date("1 June 2022 08:00:00 UTC")
+export const startDate = new Date("1 Jun 2022 18:00:00 GMT+8")
 //export const startWlDate = new Date("1 June 2022 08:00:00 UTC")
 
 export const mintPrice = '0.3'
